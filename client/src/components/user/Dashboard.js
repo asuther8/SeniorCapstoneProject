d9a464224e91aada9d992f9b64d235baad24a763
ShrimpyJ <alejbrock@gmail.com>
2022-05-05 23:39:21 -0400

    Working on auto graph

--- client/src/components/user/Dashboard.js
+++ client/src/components/user/Dashboard.js
@@ -110,6 +110,39 @@ export const options = {
 	}
 };
 
+const test = [
+  ["", "Date", "Readings", "Average", "Min", "Quartile 25", "Median", "Quartile 75", "Max", "Standard Deviation"],
+  ["1","11/01/2021 1:00 AM","12 (4%)","175","163","166.0","171.0","188.5","192","10.9"],
+  ["2","11/01/2021 2:00 AM","12 (4%)","163","153","158.0","164.0","168.0","171","5.9"],
+  ["3","11/01/2021 3:00 AM","12 (4%)","135","129","131.0","133.0","140.5","150","6.2"],
+  ["4","11/01/2021 4:00 AM","12 (4%)","125","116","117.5","129.0","130.0","130","5.9"],
+  ["5","11/01/2021 5:00 AM","12 (4%)","127","122","126.0","129.0","130.0","131","2.7"],
+  ["6","11/01/2021 6:00 AM","12 (4%)","128","123","125.0","127.0","132.0","136","4.2"],
+  ["7","11/01/2021 7:00 AM","12 (4%)","134","128","132.5","134.5","136.0","137","2.4"],
+  ["8","11/01/2021 8:00 AM","12 (4%)","127","119","121.0","122.5","132.0","149","9.2"],
+  ["9","11/01/2021 9:00 AM","12 (4%)","145","143","144.5","145.0","147.5","149","1.9"],
+  ["10","11/01/2021 10:00 AM","12 (4%)","135","131","133.0","134.0","139.5","143","3.9"],
+  ["11","11/01/2021 11:00 AM","12 (4%)","135","131","133.0","134.5","139.5","143","4"],
+  ["12","11/01/2021 12:00 PM","12 (4%)","126","113","117.0","125.5","137.0","141","10"],
+  ["13","11/01/2021 1:00 PM","12 (4%)","147","116","130.5","153.0","165.0","168","18.5"],
+  ["14","11/01/2021 2:00 PM","12 (4%)","144","84","104.5","155.0","180.0","190","38.9"],
+  ["15","11/01/2021 3:00 PM","12 (4%)","177","156","159.0","179.0","195.5","200","16.6"],
+  ["16","11/01/2021 4:00 PM","12 (4%)","213","196","207.0","212.5","223.5","228","9.5"],
+  ["17","11/01/2021 5:00 PM","12 (4%)","212","206","209.5","211.5","214.0","218","3.5"],
+  ["18","11/01/2021 6:00 PM","12 (4%)","203","199","200.5","202.5","206.5","212","4"],
+  ["19","11/01/2021 7:00 PM","12 (4%)","204","179","196.0","209.0","214.0","216","11.5"],
+  ["20","11/01/2021 8:00 PM","12 (4%)","167","162","163.5","166.0","172.5","175","4.5"],
+  ["21","11/01/2021 9:00 PM","12 (4%)","194","161","169.5","196.0","216.0","229","24.6"],
+  ["22","11/01/2021 10:00 PM","12 (4%)","175","157","159.0","161.5","195.0","209","19.7"],
+  ["23","11/01/2021 11:00 PM","12 (4%)","160","154","156.5","159.5","164.0","167","4.1"],
+  ["24","11/02/2021 12:00 AM","12 (4%)","173","155","157.0","174.0","188.5","189","15.1"],
+  ["25","11/02/2021 1:00 AM","12 (4%)","247","223","226.0","246.0","268.5","273","19.6"],
+  ["26","11/02/2021 2:00 AM","12 (4%)","251","246","248.0","251.0","253.5","260","3.8"],
+  ["27","11/02/2021 3:00 AM","12 (4%)","246","243","246.5","247.0","248.0","250","1.8"],
+  ["28","11/02/2021 4:00 AM","12 (4%)","240","233","235.0","241.5","244.5","245","4.5"],
+  ["29","11/02/2021 5:00 AM","12 (4%)","220","213","214.5","217.0","227.0","236","8.4"],
+  ["30","11/02/2021 6:00 AM","12 (4%)","208","187","205.0","213.0","214.0","215","8.8"]
+];  
 
 //Globals used in several functions
 var labels = [];
@@ -186,15 +219,17 @@ export function draw() {
 		backgroundColor: 'rgba(132, 255, 99, 0.5)'
 	};
 
+  /*
 	if (c1 != true && c2 != true && c3 != true) {
 		alert("Please check at least one data type.");
 		return;
 	}
+  */
 
 	//Adds the data to the graph's set if the appropriate checkbox is clicked
-	if (c1) {
+	//if (c1) {
 		dsets.push(cavg);
-	}
+	//}
 
 	if (c2) {
 		dsets.push(cmin);
@@ -260,45 +295,95 @@ const fetchData = async (e) => {
   });
 }
 
+const getData = async(e) => {
+  e.preventDefault();
+  tdat = test;
+  labels = arrayColumn(test, 1);
+  labels = labels.slice(1, (labels.length - 1));
+  lsize = labels.length;
+  draw();
+}
+
 export default function Dashboard() {
   const [email, setEmail] = useState("");
   const [username, getUsername] = useState(localStorage.getItem('user'));
   
 	const { CSVReader } = useCSVReader();
 
-  const handleSubmitFile = async (e) => {
-    e.preventDefault();
-    let result = await fetch(
-    'http://localhost:8082/upload', {
-        method: "post",
-        body: JSON.stringify({ username }),
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    }).then(response => {
-      if (response.ok) {
-          return response.json();
-      }
-      throw response;
-    }).then(data => {
-      if (data === false) {
-        alert("Upload error");
-      }
-      else {
-        alert("Upload successful");
-        //window.location.replace("/dashboard");
-      }
-    }).catch(error => {
-      console.error(error);
-    }).finally(() => {
-    });
-}
+	var dsets = [];
+	var lset = labels;
+
+	var sub = 24;
+	var glen;
+
+	glen = lsize - sub;
+
+	if (glen < 0) {
+		alert("Not enough user data. Please select a shorter time span.");
+		return;
+	}
+
+	lset = lset.slice(glen, lsize);
+	console.log(lset);
+
+	//Data for the graph
+	var cavg = {
+		label: "Average",
+		data: arrayColumn(tdat, 3).slice(glen, lsize),
+		borderColor: 'rgb(255, 99, 132)',
+		backgroundColor: 'rgba(255, 99, 132, 0.5)'
+	};
+
+	var cmin = {
+		label: "Minimum",
+		data: arrayColumn(tdat, 4).slice(glen, lsize),
+		borderColor: 'rgb(99, 132, 255)',
+		backgroundColor: 'rgba(99, 132, 255, 0.5)'
+
+	};
+
+	var cmax = {
+		label: "Maximum",
+		data: arrayColumn(tdat, 8).slice(glen, lsize),
+		borderColor: 'rgb(132, 255, 99)',
+		backgroundColor: 'rgba(132, 255, 99, 0.5)'
+	};
+
+  /*
+	if (c1 != true && c2 != true && c3 != true) {
+		alert("Please check at least one data type.");
+		return;
+	}
+  */
+
+	data = {
+		labels: lset,
+		datasets: dsets
+	};
+
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+
+  /*
+	if (w < 1000) {
+		w = 1200;
+	  ReactDOM.render(<div id="gg" style={{height:"150%", width: "300%"}}><Line overflow-x={"hidden"} width={w} height={h} options={options} data={data} /></div>, document.getElementById("graph"));
+	}
+
+	else {
+		ReactDOM.render(<div class="drawn" id="gg"><Line overflow-x={"hidden"} width={w} height={h} options={options} data={data} /></div>, document.getElementById("graph"));
+	}
+  */
 
   return (
-    
+    <>
     <div className="Dashboard">
-		  <>
+      <Form onSubmit={getData}>
+        <Button block size="lg" type="submit">
+          Test
+        </Button>
+      </Form>
+		  
       <CSVReader
         onUploadAccepted={(results: any) => {
           console.log('---------------------------');
@@ -327,6 +412,7 @@ export default function Dashboard() {
       </CSVReader>
 
       <div class="graph" id="graph">
+        {ReactDOM.render(<div id="gg" style={{height:"150%", width: "300%"}}><Line overflow-x={"hidden"} width={window.innderWidth} height={window.innerHeight} options={options} data={data} /></div>, document.getElementById("graph"))}
         <p>Choose your hourly data:</p>
         <div>
           <input type="checkbox" id="Avg"></input>
@@ -357,89 +443,7 @@ export default function Dashboard() {
         <p></p>
         <button onClick={draw}>Draw Graph</button>
       </div>
-      </>
     </div>
+    </>
   );
-}
-
-function GraphStart() {
-
-	const { CSVReader } = useCSVReader();
-
-	return (
-
-		<>
-
-    <Form>
-    <CSVReader
-		onUploadAccepted={(results: any) => {
-			console.log('---------------------------');
-			console.log(results);
-			console.log('---------------------------');
-			console.log(results.data[1]);
-			tdat = results.data;
-			labels = arrayColumn(results.data, 1);
-			labels = labels.slice(1, (labels.length - 1));
-			lsize = labels.length;
-			console.log(labels);
-		}}
-		>
-
-		{({
-			getRootProps,
-			acceptedFile,
-			ProgressBar,
-			getRemoveFileProps,
-		}: any) => (
-			<>
-			<div class="graph" style={styles.csvReader}>
-			<button type='button' {...getRootProps()} style={styles.browseFile}>
-			  Browse file
-			</button>
-			<div style={styles.acceptedFile}>
-			{acceptedFile && acceptedFile.name}
-			</div>
-			<button {...getRemoveFileProps()} style={styles.remove}>
-			  Remove
-			</button>
-			</div>
-			<ProgressBar style={styles.progressBarBackgroundColor} />
-			</>
-		)}
-		</CSVReader>
-
-		<div classname="graph">
-		<p>Choose your hourly data:</p>
-		<div>
-		<input type="checkbox" id="Avg"></input>
-		<label for="Avg">Average</label>
-		</div>
-
-		<div>
-		<input type="checkbox" id="Min"></input>
-		<label for="Min">Minimum</label>
-		</div>
-
-		<div>
-		<input type="checkbox" id="Max"></input>
-		<label for="Max">Maximum</label>
-		</div>
-
-		<p>Select a time range:</p>
-		<select id="trange">
-		<option value="1day">1 Day</option>
-		<option value="3day">3 Days</option>
-		<option value="1week">1 Week</option>
-		<option value="2week">2 Weeks</option>
-		<option value="1month">1 Month</option>
-		<option value="2month">2 Months</option>
-		<option value="3month">3 Months</option>
-		</select>
-
-		<p></p>
-		<button onClick={draw}>Draw Graph</button>
-		</div>
-    </Form>
-		</>
-	);
 }
